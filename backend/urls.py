f849d71afb8a3d168e2786e50be892f2a8645660
aobrant <aobrant75@gmail.com>
2023-05-11 02:31:51 +0300

    Import data, http responses

--- backend/urls.py
+++ backend/urls.py
@@ -2,10 +2,11 @@ from django.db import router
 from django.urls import path, include
 from rest_framework import routers
 
-from backend.views import PartnerUpdate
+from backend.views import PartnerUpdate, LocalPartnerUpdate
 
 app_name = 'backend'
 
 urlpatterns = [
     path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
+    path('partner/update/local', LocalPartnerUpdate.as_view(), name='Local_partner-update'),
 ]
--- backend/views.py
+++ backend/views.py
@@ -1,8 +1,12 @@
+import yaml
 from django.core.validators import URLValidator
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+import os
+from urllib.request import urlopen
 from django.shortcuts import render
 from requests import get
 from rest_framework.views import APIView
+import json
 from yaml import load as load_yaml, Loader
 from django.core.exceptions import ValidationError
 
@@ -11,18 +15,45 @@ from backend.models import Shop, Category, ProductInfo, Product, ProductParamete
 
 # Create your views here.
 
+def import_partner(data):
+    shop, _ = Shop.objects.get_or_create(name=data['shop'], user_id=request.user.id)
+    for category in data['categories']:
+        category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
+        category_object.shops.add(shop.id)
+        category_object.save()
+    ProductInfo.objects.filter(shop_id=shop.id).delete()
+    for item in data['goods']:
+        product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
+
+        product_info = ProductInfo.objects.create(product_id=product.id,
+                                                  external_id=item['id'],
+                                                  model=item['model'],
+                                                  price=item['price'],
+                                                  price_rrc=item['price_rrc'],
+                                                  quantity=item['quantity'],
+                                                  shop_id=shop.id)
+        for name, value in item['parameters'].items():
+            parameter_object, _ = Parameter.objects.get_or_create(name=name)
+            ProductParameter.objects.create(product_info_id=product_info.id,
+                                            parameter_id=parameter_object.id,
+                                            value=value)
+        return
+
+
 class PartnerUpdate(APIView):
     """
     Класс для обновления прайса от поставщика
     """
+
     def post(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return JsonResponse({'Status': False, 'Error': 'Log in required'}, status=403)
 
         if request.user.type != 'shop':
-            return JsonResponse({'Status': False, 'Error': 'Only for shops'}, status=403)
+            return JsonResponse({'Status': False, 'Error': 'Только для магазинов'}, status=403)
 
         url = request.data.get('url')
+
         if url:
             validate_url = URLValidator()
             try:
@@ -31,31 +62,25 @@ class PartnerUpdate(APIView):
                 return JsonResponse({'Status': False, 'Error': str(e)})
             else:
                 stream = get(url).content
-
                 data = load_yaml(stream, Loader=Loader)
 
-                shop, _ = Shop.objects.get_or_create(name=data['shop'], user_id=request.user.id)
-                for category in data['categories']:
-                    category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
-                    category_object.shops.add(shop.id)
-                    category_object.save()
-                ProductInfo.objects.filter(shop_id=shop.id).delete()
-                for item in data['goods']:
-                    product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
+                import_partner(data)
 
-                    product_info = ProductInfo.objects.create(product_id=product.id,
-                                                              external_id=item['id'],
-                                                              model=item['model'],
-                                                              price=item['price'],
-                                                              price_rrc=item['price_rrc'],
-                                                              quantity=item['quantity'],
-                                                              shop_id=shop.id)
-                    for name, value in item['parameters'].items():
-                        parameter_object, _ = Parameter.objects.get_or_create(name=name)
-                        ProductParameter.objects.create(product_info_id=product_info.id,
-                                                        parameter_id=parameter_object.id,
-                                                        value=value)
+                # return JsonResponse({'Status': True})
+                return HttpResponse(json.dumps({'Status': True}), content_type='application/json')
 
-                return JsonResponse({'Status': True})
+        # return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
+        return HttpResponse(json.dumps({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'}),
+                            content_type='application/json')
 
-        return JsonResponse({'Status': False, 'Errors': 'Some required arguments not provided'})
+
+class LocalPartnerUpdate(APIView):
+    """
+        Класс для обновления прайса от поставщика через локальный файл в папке fixtures
+        """
+    def post(self, request, *args, **kwargs):
+        file_path = os.path.join(os.path.dirname(__file__), 'fixtures', 'shop1.yaml')
+        with open(file_path, 'r') as stream:
+            data = yaml.safe_load(stream)
+        import_partner(data)
+        return HttpResponse(json.dumps({'Status': True}), content_type='application/json')
